d600198f4b8aba6c0e8f222eb96010e903418462
Maciej Lipski <lipskimaciej170@gmail.com>
2022-10-17 21:32:21 +0200

    vowels to z

--- Lab2.java
+++ Lab2.java
@@ -1,7 +1,18 @@
 public class Lab2 {
     public static void main(String[] args) {
+        String imie = args[0];
+        String pseudomin = args[1];
         System.out.println("Hello world");
-        System.out.println(args[0]+" "+args[1]);
+        System.out.println(Podmiana(imie)+" "+Podmiana(pseudomin));
+
+    }
+    public static String Podmiana (String podaneslowo){
+        String zmienioneslowo=podaneslowo.replace('a','z');
+        zmienioneslowo=zmienioneslowo.replace('e','z');
+        zmienioneslowo=zmienioneslowo.replace('i','z');
+        zmienioneslowo=zmienioneslowo.replace('u','z');
+        zmienioneslowo=zmienioneslowo.replace('o','z');
+        return zmienioneslowo;
     }
 
 }

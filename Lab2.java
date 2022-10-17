5a16e95a356ea19173f0714ec5ddcdcd1da058a4
Maciej Lipski <lipskimaciej170@gmail.com>
2022-10-17 21:47:34 +0200

    Zmiana miejsca imienia

--- Lab2.java
+++ Lab2.java
@@ -1,7 +1,11 @@
 public class Lab2 {
     public static void main(String[] args) {
+        String imie = args[0];
+        String pseudomin = args[1];
         System.out.println("Hello world");
-        System.out.println(args[0]+" "+args[1]);
+        System.out.println(pseudomin+" "+imie);
+
     }
+
 
 }

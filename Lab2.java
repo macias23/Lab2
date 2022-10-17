baf5d7a983672a50d82e2d3566289e485fe7c4b5
Maciej Lipski <maciejlipski@Air-Maciej.home>
2022-10-17 21:02:53 +0200

    Wypisuje imię i pseudomin

--- Lab2.java
+++ Lab2.java
@@ -1,5 +1,7 @@
 public class Lab2 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello world");
+        System.out.println(args[0]+" "+args[1]);
     }
+
 }
